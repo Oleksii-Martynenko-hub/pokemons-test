2cad153fc3a48dc1ce6df14c687e768f8022d5f8
Oleksii Martynenko <oleksii.martynenko.official@gmail.com>
2023-06-16 11:14:15 +0300

    Added global type for env var of image storage

--- src/@types/react-app-env.d.ts
+++ src/@types/react-app-env.d.ts
@@ -3,5 +3,6 @@ declare namespace NodeJS {
     NODE_ENV: 'development' | 'production' | 'test';
     PUBLIC_URL: string;
     REACT_APP_API_URL: string;
+    REACT_APP_IMAGE_STORAGE_URL: string;
   }
 }
