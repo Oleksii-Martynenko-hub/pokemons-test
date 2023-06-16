8ca4ffb6f0c8b0c176fee7b940d6317e5d7f96df
Oleksii Martynenko <oleksii.martynenko.official@gmail.com>
2023-06-16 12:50:04 +0300

    Fixed definition of environment variables in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,43 +1,54 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsConfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig({
-  cacheDir: './node_modules/.vite/pokemons-test',
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
 
-  server: {
-    port: 4200,
-    host: 'localhost',
-  },
-
-  preview: {
-    port: 4300,
-    host: 'localhost',
-  },
-
-  plugins: [
-    react(),
-    viteTsConfigPaths({
-      root: './',
-    }),
-  ],
-
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [
-  //    viteTsConfigPaths({
-  //      root: './',
-  //    }),
-  //  ],
-  // },
-
-  test: {
-    globals: true,
-    cache: {
-      dir: './node_modules/.vitest',
+  return {
+    cacheDir: './node_modules/.vite/pokemons-test',
+    define: {
+      'process.env': {
+        REACT_APP_NODE_ENV: env.REACT_APP_NODE_ENV,
+        REACT_APP_API_URL: env.REACT_APP_API_URL,
+        REACT_APP_IMAGE_STORAGE_URL: env.REACT_APP_IMAGE_STORAGE_URL,
+      },
     },
-    environment: 'jsdom',
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-  },
+
+    server: {
+      port: 4200,
+      host: 'localhost',
+    },
+
+    preview: {
+      port: 4300,
+      host: 'localhost',
+    },
+
+    plugins: [
+      react(),
+      viteTsConfigPaths({
+        root: './',
+      }),
+    ],
+
+    // Uncomment this if you are using workers.
+    // worker: {
+    //  plugins: [
+    //    viteTsConfigPaths({
+    //      root: './',
+    //    }),
+    //  ],
+    // },
+
+    test: {
+      globals: true,
+      cache: {
+        dir: './node_modules/.vitest',
+      },
+      environment: 'jsdom',
+      include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    },
+  };
 });
