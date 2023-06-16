74e747165fb274b04169bf70fcc090cbeceb57ff
Oleksii Martynenko <oleksii.martynenko.official@gmail.com>
2023-06-16 12:55:17 +0300

    Added test pokemon list to home page

--- src/pages/home-page/home-page.tsx
+++ src/pages/home-page/home-page.tsx
@@ -1,4 +1,14 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
+
+import { APIStatus } from 'src/api/MainApi';
+
+import { AppDispatch } from 'src/store';
+import { getPokemonDataAsync } from 'src/store/pokemon/actions';
+import { selectPokemonData, selectStatus } from 'src/store/pokemon/selectors';
+
+import { FullPageLoader } from 'src/components/common/FullPageLoader';
 
 /* eslint-disable-next-line */
 export interface HomePageProps {}
@@ -8,9 +18,34 @@ const StyledHomePage = styled.div`
 `;
 
 export function HomePage(props: HomePageProps) {
+  const dispatch = useDispatch<AppDispatch>();
+  const pokemonData = useSelector(selectPokemonData);
+  const status = useSelector(selectStatus);
+
+  useEffect(() => {
+    if (status === APIStatus.IDLE) {
+      dispatch(getPokemonDataAsync());
+    }
+  }, []);
+
+  if (status === APIStatus.PENDING) {
+    return <FullPageLoader />;
+  }
   return (
     <StyledHomePage>
       <h1>Welcome to HomePage!</h1>
+
+      {pokemonData && (
+        <li>
+          {pokemonData.map(({ id, name, imageUrl }) => (
+            <li key={id}>
+              <img src={imageUrl} alt={name} />
+              <span>{name}</span>
+              <hr />
+            </li>
+          ))}
+        </li>
+      )}
     </StyledHomePage>
   );
 }
